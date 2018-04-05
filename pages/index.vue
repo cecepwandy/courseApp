8cb44f8c967d9b312a76b8e914bea913e9892135
cecep wandy <cecep.wandy@giftcard.co.id>
2018-04-05 17:56:07 +0700

    banana

---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="home-page">
+		<section class="intro">
+			<h1>Get the lastest tech news! </h1>
+		</section>
+		<section class="featured-posts">
+			<PostPreview 
+		 id="1"
+	 thumbnail="https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&h=350"
+	title="hello there"
+	previewText="this my fist Post"/>
+			<PostPreview 
+		 id="2"
+	 thumbnail="https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&h=350"
+	title="jajaja"
+	previewText="this my fist Post"/>
+			<PostPreview 
+		 id="3"
+	 thumbnail="https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&h=350"
+	title="banana"
+	previewText="this my fist Post"/>
+		</section>
+	</div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+	components: {
+		PostPreview
+	}
+}
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+	background-image: url('~assets/images/earthmilkyway.jpg');
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
